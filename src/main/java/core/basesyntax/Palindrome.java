f4b9a93a5b2c5305f21f4a6053b4888ad3759be5
Filipp <filipp888@gmail.com>
2020-04-01 13:28:49 +0200

    Added solution for Task 3.3

--- src/main/java/core/basesyntax/Palindrome.java
+++ src/main/java/core/basesyntax/Palindrome.java
@@ -21,6 +21,8 @@ public class Palindrome {
      * <p>Результат: true</p>
      */
     public boolean isPalindrome(String text) {
-        return false;
+        StringBuilder stringBuilder = new StringBuilder(text.toLowerCase());
+        return stringBuilder.toString().replaceAll("\\W", "")
+                .equals(stringBuilder.reverse().toString().replaceAll("\\W", ""));
     }
 }
